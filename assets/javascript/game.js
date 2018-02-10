1be7f70089453fc6bc96d788a0cc753be9ef7c56
Jon-Erik <jdc151@case.edu>
2018-02-10 15:31:41 -0500

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,27 @@ var wins = 0;
 var remainingGuesses = 16;
 var currentWord = [];
 var lettersGuessed = "";
-var gameStarted = true;
-var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+var gameStarted = false;
+var computerChoice = computerOptions[0];//[Math.floor(Math.random() * computerOptions.length)];
     console.log(computerChoice);
 var remainingLetters = computerChoice.length;
 var alreadyGuessed = "You already guessed that letter.";
-var winMessage = "You win! Click ? to play again";
-var winContent = document.getElementById('messagebox').textContent;
+var winMessage = "You win! Press any key to play again";
+var winContent = false //document.getElementById('messagebox').textContent;
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var html = function() {
+        
+        var gameDisplay = "<p>Wins: " + wins + "</p>" +
+        "<p>Current Word: <br>" + currentWord.join(" ") + "</p>" +
+        "<p>Remaining Guesses: " + remainingGuesses + "</p>" +
+        "<p>Letters Already Guessed: " + lettersGuessed + "</p>";
+
+    document.querySelector('#game').innerHTML = gameDisplay;
+}
 
   document.onkeyup = function(event) {
- 	if (gameStarted === true) {
+ 	if (gameStarted === false) {
     
     var userGuess = event.key;
     console.log('user guess is ' + userGuess);
@@ -25,78 +36,84 @@ var winContent = document.getElementById('messagebox').textContent;
     	currentWord.push(" _");
     }
 
-    gameStarted = false;
+    gameStarted = true;
  	console.log(gameStarted);
+    
+    html();
 
-} else if (gameStarted === false); {
+    return;
+
+} else if (gameStarted === true); {
 	
     var userGuess = event.key;
     console.log('user guess is ' + userGuess);
 
+    if (alphabet.includes(userGuess.toUpperCase())) {
+            //document.querySelector('#messagebox').innerHTML = 
+            //'Please enter a valid letter.';
 
-    if (computerChoice.includes(userGuess.toUpperCase())) {
-    	console.log('match');
-    	
-        if (lettersGuessed.includes(userGuess)) {
-            document.querySelector('#messagebox').innerHTML = 
-            alreadyGuessed;
+        if (computerChoice.includes(userGuess.toUpperCase())) {
+        	console.log('match');
+        	
+            if (lettersGuessed.includes(userGuess)) {
+                document.querySelector('#messagebox').innerHTML = 
+                alreadyGuessed;
 
-            remainingGuesses = remainingGuesses - 1;
+            } else {
+                lettersGuessed = lettersGuessed + userGuess + " ";
+               
+               remainingGuesses = remainingGuesses - 1; 
+
+                document.querySelector('#messagebox').innerHTML = 
+                'Match!';
+
+            	for (var j = 0; j < computerChoice.length; j++) {
+        			if (computerChoice[j] === userGuess.toUpperCase()) {
+        			     currentWord[j] = userGuess.toUpperCase();
+                        remainingLetters = remainingLetters -1;
+                        console.log('remaining letters: ' + remainingLetters);
+                        if (remainingLetters === 0) {
+                            wins = wins + 1;
+                            document.querySelector('#messagebox').innerHTML = 
+                            winMessage;
+                            winContent = true
+                                if (winContent && computerChoice === 'BACH') {
+                            
+                                        document.getElementById("image").setAttribute
+                                        ("src", "assets/images/Johann_Sebastian_Bach.jpg");
+                                } else {
+
+                                }
+                            gameStarted = false;
+                            currentWord = [];
+                            computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+                            console.log(computerChoice);
+                            lettersGuessed = "";
+                            remainingLetters = computerChoice.length;
+        			     }
+        			}
+                }
+            }   
+        } else if (lettersGuessed.includes(userGuess)) {
+        	document.querySelector('#messagebox').innerHTML = 
+            "You already guessed that letter.";
+                console.log('no match');
+                
+
         } else {
-            lettersGuessed = lettersGuessed + userGuess + " ";
-           
-           remainingGuesses = remainingGuesses - 1; 
-
             document.querySelector('#messagebox').innerHTML = 
-            'Match!';
+            'No match. Try Again!';
 
-        	for (var j = 0; j < computerChoice.length; j++) {
-    			if (computerChoice[j] === userGuess.toUpperCase()) {
-    			     currentWord[j] = userGuess.toUpperCase();
-                    remainingLetters = remainingLetters -1;
-                    console.log('remaining letters: ' + remainingLetters);
-                    if (remainingLetters === 0) {
-                        wins = wins + 1;
-                        document.querySelector('#messagebox').innerHTML = 
-                        winMessage;
-                            if (winContent === winMessage && computerChoice === 'BACH') {
-                                    document.getElementById("image").setAttribute("src", "assets/images/Johann_Sebastian_Bach.jpg");;
-                            } else {
+            lettersGuessed = lettersGuessed + userGuess + " ";
+            remainingGuesses = remainingGuesses - 1;    
+        }
 
-                            }
-                        gameStarted === true;
-    			     }
-    			}
-            }
-        }   
-    } else if (lettersGuessed.includes(userGuess)) {
-    	document.querySelector('#messagebox').innerHTML = 
-        "You already guessed that letter.";
-            console.log('no match');
-        remainingGuesses = remainingGuesses - 1;    
-
-    } else if (userGuess === "Shift" ||
-                   userGuess === "Alt" ||
-                   userGuess === "Enter" ||
-                   userGuess === "(" ||
-                   userGuess === ")" ||
-                   userGuess === "|" ||
-                   userGuess === "/") {
-            alert('Please enter a valid letter.');
     } else {
         document.querySelector('#messagebox').innerHTML = 
-        'No match. Try Again!';
-
-        lettersGuessed = lettersGuessed + userGuess + " ";
-        remainingGuesses = remainingGuesses - 1;    
+            'Please enter a valid letter.';
     }
 
-    var html = 
-    	"<p>Wins: " + wins + "</p>" +
-    	"<p>Current Word: <br>" + currentWord + "</p>" +
-    	"<p>Remaining Guesses: " + remainingGuesses + "</p>" +
-    	"<p>Letters Already Guessed: " + lettersGuessed + "</p>";
+    html();
 
-    document.querySelector('#game').innerHTML = html;
     }
 }
