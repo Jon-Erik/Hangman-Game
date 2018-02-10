553f199dd7f09787197053d58412d683c4e9a001
Jon-Erik <jdc151@case.edu>
2018-02-09 19:26:15 -0500

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,15 +8,18 @@ var gameStarted = true;
 var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
     console.log(computerChoice);
 var remainingLetters = computerChoice.length;
+var alreadyGuessed = "You already guessed that letter.";
+var winMessage = "You win! Click ? to play again";
+var winContent = document.getElementById('messagebox').textContent;
 
   document.onkeyup = function(event) {
  	if (gameStarted === true) {
     
     var userGuess = event.key;
-    console.log(userGuess);
+    console.log('user guess is ' + userGuess);
 
     var wordLength = computerChoice.length;
-    console.log(wordLength);
+    console.log('word length is ' + wordLength);
 
     for (i = 0; i < wordLength; i++) {
     	currentWord.push(" _");
@@ -28,28 +31,50 @@ var remainingLetters = computerChoice.length;
 } else if (gameStarted === false); {
 	
     var userGuess = event.key;
-    console.log(userGuess);
+    console.log('user guess is ' + userGuess);
 
 
     if (computerChoice.includes(userGuess.toUpperCase())) {
     	console.log('match');
     	
-    	for (var j = 0; j < computerChoice.length; j++) {
-			if (computerChoice[j] === userGuess.toUpperCase()) {
-			     currentWord[j] = userGuess.toUpperCase();
-                remainingLetters = remainingLetters -1;
-                console.log(remainingLetters);
-                if (remainingLetters === 0) {
-                    wins = wins + 1;
-                    alert("You win!");
-                    confirm("Click OK to play again!");
-                    gameStarted === true;
-			     }
-			}
-        }
+        if (lettersGuessed.includes(userGuess)) {
+            document.querySelector('#messagebox').innerHTML = 
+            alreadyGuessed;
+
+            remainingGuesses = remainingGuesses - 1;
+        } else {
+            lettersGuessed = lettersGuessed + userGuess + " ";
+           
+           remainingGuesses = remainingGuesses - 1; 
+
+            document.querySelector('#messagebox').innerHTML = 
+            'Match!';
+
+        	for (var j = 0; j < computerChoice.length; j++) {
+    			if (computerChoice[j] === userGuess.toUpperCase()) {
+    			     currentWord[j] = userGuess.toUpperCase();
+                    remainingLetters = remainingLetters -1;
+                    console.log('remaining letters: ' + remainingLetters);
+                    if (remainingLetters === 0) {
+                        wins = wins + 1;
+                        document.querySelector('#messagebox').innerHTML = 
+                        winMessage;
+                            if (winContent === winMessage && computerChoice === 'BACH') {
+                                    document.getElementById("image").setAttribute("src", "assets/images/Johann_Sebastian_Bach.jpg");;
+                            } else {
+
+                            }
+                        gameStarted === true;
+    			     }
+    			}
+            }
+        }   
     } else if (lettersGuessed.includes(userGuess)) {
-    		alert('You already guessed that letter.');
+    	document.querySelector('#messagebox').innerHTML = 
+        "You already guessed that letter.";
             console.log('no match');
+        remainingGuesses = remainingGuesses - 1;    
+
     } else if (userGuess === "Shift" ||
                    userGuess === "Alt" ||
                    userGuess === "Enter" ||
@@ -59,6 +84,9 @@ var remainingLetters = computerChoice.length;
                    userGuess === "/") {
             alert('Please enter a valid letter.');
     } else {
+        document.querySelector('#messagebox').innerHTML = 
+        'No match. Try Again!';
+
         lettersGuessed = lettersGuessed + userGuess + " ";
         remainingGuesses = remainingGuesses - 1;    
     }
