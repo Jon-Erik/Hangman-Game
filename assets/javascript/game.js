21264d86babc48e05f3b34e80a4b6947ae790fbb
Jon-Erik <jdc151@case.edu>
2018-02-12 11:53:02 -0500

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,12 @@ var remainingGuesses = 16;
 var currentWord = [];
 var lettersGuessed = "";
 var gameStarted = false;
-var computerChoice = computerOptions[0];//[Math.floor(Math.random() * computerOptions.length)];
+var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
     console.log(computerChoice);
 var remainingLetters = computerChoice.length;
 var alreadyGuessed = "You already guessed that letter.";
 var winMessage = "You win! Press any key to play again";
-var winContent = false //document.getElementById('messagebox').textContent;
+var winContent = false;
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
                 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var html = function() {
@@ -24,7 +24,8 @@ var html = function() {
 }
 
   document.onkeyup = function(event) {
- 	if (gameStarted === false) {
+
+ if (gameStarted === false) {
     
     var userGuess = event.key;
     console.log('user guess is ' + userGuess);
@@ -41,10 +42,16 @@ var html = function() {
     
     html();
 
+    document.getElementById("image").setAttribute("src", "assets/images/question mark head.jpg");
+
+    document.querySelector('#caption').innerHTML = "Who could it be?";
+
+    document.querySelector('#messagebox').innerHTML = "Press a letter to starting guessing again";
+
     return;
 
 } else if (gameStarted === true); {
-	
+
     var userGuess = event.key;
     console.log('user guess is ' + userGuess);
 
@@ -62,44 +69,73 @@ var html = function() {
             } else {
                 lettersGuessed = lettersGuessed + userGuess + " ";
                
-               remainingGuesses = remainingGuesses - 1; 
+                remainingGuesses = remainingGuesses - 1; 
 
                 document.querySelector('#messagebox').innerHTML = 
                 'Match!';
 
-            	for (var j = 0; j < computerChoice.length; j++) {
-        			if (computerChoice[j] === userGuess.toUpperCase()) {
-        			     currentWord[j] = userGuess.toUpperCase();
-                        remainingLetters = remainingLetters -1;
-                        console.log('remaining letters: ' + remainingLetters);
-                        if (remainingLetters === 0) {
-                            wins = wins + 1;
-                            document.querySelector('#messagebox').innerHTML = 
-                            winMessage;
-                            winContent = true
-                                if (winContent && computerChoice === 'BACH') {
-                            
-                                        document.getElementById("image").setAttribute
-                                        ("src", "assets/images/Johann_Sebastian_Bach.jpg");
-                                } else {
-
+                if (remainingGuesses === 0) {
+                    //document.querySelector('#game').innerHTML = 
+                    //"Game Over! Total wins: " + wins;
+                    //document.querySelector('#messagebox').innerHTML = 
+                   // "Thanks for playing! Refresh the page to start over";
+                } else {
+                    for (var j = 0; j < computerChoice.length; j++) {
+                		if (computerChoice[j] === userGuess.toUpperCase()) {
+                            currentWord[j] = userGuess.toUpperCase();
+                            remainingLetters = remainingLetters -1;
+                            console.log('remaining letters: ' + remainingLetters);
+                                if (remainingLetters === 0) {
+                                    wins = wins + 1;
+                                    document.querySelector('#messagebox').innerHTML = 
+                                    winMessage;
+                                    winContent = true;
+                                        if (winContent && computerChoice === 'BACH') {
+                                                document.getElementById("image").setAttribute
+                                                ("src", "assets/images/Johann_Sebastian_Bach.jpg");
+                                                document.querySelector('#caption').innerHTML = 
+                                                "Johann Sebastian Bach, 1685-1750";
+                                        } else if (winContent && computerChoice === 'BEETHOVEN') {
+                                                document.getElementById("image").setAttribute
+                                                ("src", "assets/images/beethoven.jpg");
+                                                document.querySelector('#caption').innerHTML = 
+                                                "Ludwig van Beethoven, 1770-1827";
+                                        } else if (winContent && computerChoice === 'BRAHMS') {
+                                                document.getElementById("image").setAttribute
+                                                ("src", "assets/images/JohannesBrahms.jpg");
+                                                document.querySelector('#caption').innerHTML = 
+                                                "Johannes Brahms, 1833-1897";
+                                        } else if (winContent && computerChoice === 'SCHUBERT') {
+                                                document.getElementById("image").setAttribute
+                                                ("src", "assets/images/schubert.jpg");
+                                                document.querySelector('#caption').innerHTML = 
+                                                "Franz Schubert, 1797-1828";
+                                        } else if (winContent && computerChoice === 'SCHUMANN') {
+                                                document.getElementById("image").setAttribute
+                                                ("src", "assets/images/Schumann.jpg");
+                                                document.querySelector('#caption').innerHTML = 
+                                                "Robert Schumann, 1810-1856";
+                                        } else if (winContent && computerChoice === 'VIVALDI') {
+                                                document.getElementById("image").setAttribute
+                                                ("src", "assets/images/vivaldi.jpg");
+                                                document.querySelector('#caption').innerHTML = 
+                                                "Antonio Vivaldi, 1678-1741";
+                                        }
+                                    gameStarted = false;
+                                    currentWord = [];
+                                    computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+                                    console.log(computerChoice);
+                                    lettersGuessed = "";
+                                    remainingLetters = computerChoice.length;
                                 }
-                            gameStarted = false;
-                            currentWord = [];
-                            computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-                            console.log(computerChoice);
-                            lettersGuessed = "";
-                            remainingLetters = computerChoice.length;
+                            }
         			     }
-        			}
+                    }
                 }
-            }   
         } else if (lettersGuessed.includes(userGuess)) {
-        	document.querySelector('#messagebox').innerHTML = 
-            "You already guessed that letter.";
-                console.log('no match');
-                
-
+                document.querySelector('#messagebox').innerHTML = 
+                "You already guessed that letter.";
+                    console.log('no match');
         } else {
             document.querySelector('#messagebox').innerHTML = 
             'No match. Try Again!';
@@ -107,13 +143,11 @@ var html = function() {
             lettersGuessed = lettersGuessed + userGuess + " ";
             remainingGuesses = remainingGuesses - 1;    
         }
-
     } else {
-        document.querySelector('#messagebox').innerHTML = 
-            'Please enter a valid letter.';
+    document.querySelector('#messagebox').innerHTML = 
+        'Please enter a valid letter.';
     }
 
     html();
-
     }
 }
